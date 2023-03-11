17c15c81f12f3b6ea2bf736281b710fcb1550525
petrovi4 <40.10.444@gmail.com>
2023-03-11 15:19:09 +0300

    Merge remote-tracking branch 'origin/master'

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
